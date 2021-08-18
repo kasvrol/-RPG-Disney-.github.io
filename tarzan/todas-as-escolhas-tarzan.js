bf18976b98d3d4dd7db6b7d984dda9f21023d97f
Ana Karolina dos Santos <88169439+kasvrol@users.noreply.github.com>
2021-08-18 15:58:37 -0300

    Add files via upload
    "Mudando imagem da fase 2, e mudança do javascript para achar as páginas fora da pasta"

--- tarzan/todas-as-escolhas-tarzan.js
+++ tarzan/todas-as-escolhas-tarzan.js
@@ -6,46 +6,36 @@ function tarzan01() {
             break;
         }
         else if (escolha ==2) {
-            location.assign('gameover-tarzan.html');
-            break;
-        }
-        else {
-            alert ('Apenas digite 1 ou 2!');
-            break;
-        }
+            location.assign('../gameover/gameover-tarzan.html');
+            break;}
+        alert ('Apenas digite 1 ou 2!');
     }
 }
 function tarzan02() {
     while (true) {
         var escolha = prompt("1 - Tarzan se aproximar ao máximo da mãe dos filhotes para os devolver e ela ver que ele não os fará mal. \n 2 - Tarzan deixa os filhotes no chão para a mãe não o atacar e foge.");
         if(escolha ==1){
-            location.assign('gameover-tarzan.html');
+            location.assign('../gameover/gameover-tarzan.html');
             break;
         }
         else if (escolha ==2) {
             location.assign('Tarzan03.html');
             break;
         }
-        else {
-            alert ('Apenas digite 1 ou 2!');
-            break;
-        }
+        alert ('Apenas digite 1 ou 2!');
     }
 }
 function tarzan03() {
     while (true) {
         var escolha = prompt("1 - Tarzan dá seu berro para chamar Jane e ver se ela está segura fora da barraca.\n 2 - Tarzan entra na barraca a procura de Jane.");
         if(escolha ==1){
-            location.assign('vitoria-tarzan.html');
+            location.assign('../vitoria/vitoria-tarzan.html');
             break;
         }
         else if (escolha == 2) {
-            location.assign('gameover-tarzan.html');
+            location.assign('../gameover/gameover-tarzan.html');
             break;
         }
-        else {
-            alert ('Apenas digite 1 ou 2!');
-            break;
-        }
+        alert ('Apenas digite 1 ou 2!');
     }
 }
