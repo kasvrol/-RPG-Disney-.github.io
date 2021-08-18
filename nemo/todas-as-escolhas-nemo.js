522964e588f8d70664eeffb220782d787954db82
Ana Karolina dos Santos <88169439+kasvrol@users.noreply.github.com>
2021-08-18 16:00:03 -0300

    Add files via upload
    "Mudança do javascript para achar as páginas fora da pasta"

--- nemo/todas-as-escolhas-nemo.js
+++ nemo/todas-as-escolhas-nemo.js
@@ -2,7 +2,7 @@ function nemo01() {
     while (true) {
         var escolha = prompt("1 - Marlin e Dory nadam até a barracuda e perguntam se ela viu Nemo. \n 2 - NADA! NADA! NADA, DORY!");
         if(escolha ==1){
-            location.assign('gameover-nemo.html');
+            location.assign('../gameover/gameover-nemo.html');
             break;
         }
         else if (escolha == 2) {
@@ -19,7 +19,7 @@ function nemo02() {
     while (true) {
         var escolha = prompt("1 - Tudo bem. Bruce é vegano, não precisa ter medo. \n 2 - NADA! NADA! NADA, DORY!");
         if(escolha ==1){
-            location.assign('gameover-nemo.html');
+            location.assign('../gameover/gameover-nemo.html');
             break;
         }
         else if (escolha == 2) {
@@ -37,11 +37,11 @@ function nemo03() {
     while (true) {
         var escolha = prompt("1 - Marlin e Dory berram por Nemo na frente de todos para tentar encontra-lo. \n 2 - Esperam perto dos outros pais na esperança que Nemo apareça.");
         if(escolha ==1){
-            location.assign('gameover-nemo.html');
+            location.assign('../gameover/gameover-nemo.html');
             break;
         }
         else if (escolha == 2) {
-            location.assign('vitoria-nemo.html');
+            location.assign('../vitoria/vitoria-nemo.html');
             break;
         }
         else {
